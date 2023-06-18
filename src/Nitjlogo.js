2aaa5cdef8cbd83607383b32ddbd8490482475fa
Narinder Maurya <narinder06@pm.me>
2023-06-18 15:33:10 +0530

    deleted

--- src/Nitjlogo.js
+++
@@ -1,6 +0,0 @@
-export const AcmeLogo = () => (
-    <div>
-        <img className="w-12 h-12 sm:mx-2" src="https://www.nitj.ac.in/public/assets/images/logo_250.png " alt="logo"/>
-    </div>
-  );
-  
